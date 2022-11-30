2fe5c9444281f445cfe6f9c3217984ca20d61313
sahil <HZ332893@HERTZ.NET>
2022-11-30 21:31:06 +0000

    Added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:alpine
+
+WORKDIR /usr/src/app
+
+# pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
+COPY go.mod ./
+RUN go mod download && go mod verify
+
+COPY . .
+RUN go build -v -o /usr/local/bin/app ./...
+
+RUN pwd
+RUN ls
+
+CMD ["app"]
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
-package mylogger
+package main
 
-func Hello() {
-	println("Hello From Package")
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+func main() {
+
+	body, err := os.ReadFile("data.txt")
+	if err != nil {
+		return
+	}
+	log.Printf("Data is \n%s", body)
+
+	f, _ := os.Open("./data.txt")
+	reader := csv.NewReader(f)
+	line, err := reader.ReadAll()
+	if err != nil {
+		return
+	}
+	log.Printf("Data is %s", line)
+
 }
